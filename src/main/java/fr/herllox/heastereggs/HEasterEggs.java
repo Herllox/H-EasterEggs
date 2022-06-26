cdacd09b1a0a652ce05d4fa6bff8dd9f95c7c36e
Herllox <Theo54180@yahoo.fr>
2022-06-26 23:03:32 +0200

    Initial commit

---
+++ src/main/java/fr/herllox/heastereggs/HEasterEggs.java
@@ -0,0 +1,17 @@
+package fr.herllox.heastereggs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HEasterEggs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
